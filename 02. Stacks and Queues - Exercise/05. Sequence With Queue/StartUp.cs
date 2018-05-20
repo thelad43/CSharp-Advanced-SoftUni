072fc612eb4b097a521ea06ada09736d57fe65cf
Teodor <thelad43@gmail.com>
2018-05-20 20:14:53 +0300

    Added solution for problem 05. Sequence With Queue

---
+++ 02. Stacks and Queues - Exercise/05. Sequence With Queue/StartUp.cs
@@ -0,0 +1,26 @@
+﻿namespace _05._Sequence_With_Queue
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var n = long.Parse(Console.ReadLine());
+            var queue = new Queue<long>();
+            queue.Enqueue(n);
+
+            for (int i = 0; i < 50; i++)
+            {
+                var head = queue.Peek();
+
+                queue.Enqueue(head + 1);
+                queue.Enqueue(2 * head + 1);
+                queue.Enqueue(head + 2);
+
+                Console.Write($"{queue.Dequeue()} ");
+            }
+        }
+    }
+}
