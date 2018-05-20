51dccf5af18d1d648d8616a30a19df6d5ed52aef
Teodor <thelad43@gmail.com>
2018-05-20 20:06:42 +0300

    Added solution for problem 05. Hot Potato

---
+++ 01. Stacks and Queues - Lab/05. Hot Potato/StartUp.cs
@@ -0,0 +1,33 @@
+﻿namespace _05._Hot_Potato
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var kids = Console.ReadLine().Split(' ');
+            var queue = new Queue<string>(kids);
+            var n = int.Parse(Console.ReadLine());
+
+            while (true)
+            {
+                if (queue.Count == 1)
+                {
+                    break;
+                }
+
+                for (int i = 0; i < n - 1; i++)
+                {
+                    var kid = queue.Dequeue();
+                    queue.Enqueue(kid);
+                }
+
+                Console.WriteLine($"Removed {queue.Dequeue()}");
+            }
+
+            Console.WriteLine($"Last is {queue.Dequeue()}");
+        }
+    }
+}
