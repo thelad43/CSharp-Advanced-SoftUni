8496213b052e85636ccd740af8ff9de8ef0f48dc
Teodor <thelad43@gmail.com>
2018-05-20 20:12:22 +0300

    Added solution for problem 02. Basic Stack Operations

---
+++ 02. Stacks and Queues - Exercise/02. Basic Stack Operations/StartUp.cs
@@ -0,0 +1,52 @@
+﻿namespace _02._Basic_Stack_Operations
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    partial class StartUp
+    {
+        public static void Main()
+        {
+            var inputParams = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToArray();
+
+            var countOfElementsToPush = inputParams[0];
+            var countOfElementsToPop = inputParams[1];
+            var elementForChecking = inputParams[2];
+
+            var elements = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToArray();
+
+            var stack = new Stack<int>();
+
+            for (int i = 0; i < countOfElementsToPush; i++)
+            {
+                stack.Push(elements[i]);
+            }
+
+            for (int i = 0; i < countOfElementsToPop; i++)
+            {
+                if (!stack.Any())
+                {
+                    break;
+                }
+
+                stack.Pop();
+            }
+
+            if (!stack.Any())
+            {
+                Console.WriteLine(0);
+                return;
+            }
+
+            var result = stack.Contains(elementForChecking) ? "true" : stack.Min().ToString();
+            Console.WriteLine(result);
+        }
+    }
+}
