85567b479848077228e2307335af0113c21ed35b
Teodor <thelad43@gmail.com>
2018-05-20 20:16:03 +0300

    Added solution for problem 06. Truck Tour

---
+++ 02. Stacks and Queues - Exercise/06. Truck Tour/StartUp.cs
@@ -0,0 +1,55 @@
+﻿namespace _06._Truck_Tour
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+            var queue = new Queue<int[]>();
+
+            for (int i = 0; i < n; i++)
+            {
+                var pump = Console.ReadLine()
+                    .Split(' ')
+                    .Select(int.Parse)
+                    .ToArray();
+
+                queue.Enqueue(pump);
+            }
+
+            for (int start = 0; start < n - 1; start++)
+            {
+                var fuel = 0;
+                var isSolution = true;
+
+                for (int passedPumps = 0; passedPumps < n; passedPumps++)
+                {
+                    var currentPump = queue.Dequeue();
+                    var pumpFuel = currentPump[0];
+                    var nextPumpDistance = currentPump[1];
+
+                    queue.Enqueue(currentPump);
+
+                    fuel += pumpFuel - nextPumpDistance;
+
+                    if (fuel < 0)
+                    {
+                        start += passedPumps;
+                        isSolution = false;
+                        break;
+                    }
+                }
+
+                if (isSolution)
+                {
+                    Console.WriteLine(start);
+                    Environment.Exit(0);
+                }
+            }
+        }
+    }
+}
