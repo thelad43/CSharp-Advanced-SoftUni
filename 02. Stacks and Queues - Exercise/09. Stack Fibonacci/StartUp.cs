ead9f8976f62a4c6daef236b6ff733b09643afa6
Teodor <thelad43@gmail.com>
2018-05-20 20:19:17 +0300

    Added solution for problem 09. Stack Fibonacci

---
+++ 02. Stacks and Queues - Exercise/09. Stack Fibonacci/StartUp.cs
@@ -0,0 +1,33 @@
+﻿namespace _09._Stack_Fibonacci
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+            var result = CalculateFibonacci(n);
+            Console.WriteLine(result);
+        }
+
+        private static long CalculateFibonacci(int n)
+        {
+            var stack = new Stack<long>();
+            stack.Push(0);
+            stack.Push(1);
+
+            for (int i = 1; i < n; i++)
+            {
+                var first = stack.Pop();
+                var second = stack.Pop();
+
+                stack.Push(first);
+                stack.Push(first + second);
+            }
+
+            return stack.Peek();
+        }
+    }
+}
