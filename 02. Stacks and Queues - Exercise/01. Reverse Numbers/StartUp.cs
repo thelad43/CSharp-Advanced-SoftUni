fdfe26286de5cb413403e2ac8f7305bd42f03e85
Teodor <thelad43@gmail.com>
2018-05-20 20:11:23 +0300

    Added solution for problem 01. Reverse Numbers

---
+++ 02. Stacks and Queues - Exercise/01. Reverse Numbers/StartUp.cs
@@ -0,0 +1,15 @@
+﻿namespace _01._Reverse_Numbers
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var numbers = Console.ReadLine().Split(' ');
+            var stack = new Stack<string>(numbers);
+            Console.WriteLine(string.Join(" ", stack));
+        }
+    }
+}
