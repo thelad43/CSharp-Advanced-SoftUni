af9cf1357d3f7baa3e4d136fd0c6580631d33fc6
Teodor <thelad43@gmail.com>
2018-05-20 20:05:30 +0300

    Added solution for problem 04. Matching Brackets

---
+++ 01. Stacks and Queues - Lab/04. Matching Brackets/StartUp.cs
@@ -0,0 +1,31 @@
+﻿namespace _04._Matching_Brackets
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var expression = Console.ReadLine();
+            var stack = new Stack<int>();
+
+            for (int i = 0; i < expression.Length; i++)
+            {
+                var currentChar = expression[i];
+
+                if (currentChar == '(')
+                {
+                    stack.Push(i);
+                }
+
+                if (currentChar == ')')
+                {
+                    var topMostElement = stack.Pop();
+                    var subExpression = expression.Substring(topMostElement, i - topMostElement + 1);
+                    Console.WriteLine(subExpression);
+                }
+            }
+        }
+    }
+}
