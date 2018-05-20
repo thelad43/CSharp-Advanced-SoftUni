bd479c0f0be6a74dee88c99367f8586d63ba1aa3
Teodor <thelad43@gmail.com>
2018-05-20 19:59:23 +0300

    Added solution for problem 01. Reverse Strings

---
+++ 01. Stacks and Queues - Lab/01. Reverse Strings/StartUp.cs
@@ -0,0 +1,24 @@
+﻿namespace _01._Reverse_Strings
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var inputAsCharArray = Console.ReadLine().ToCharArray();
+            var stack = new Stack<char>();
+
+            foreach (var letter in inputAsCharArray)
+            {
+                stack.Push(letter);
+            }
+
+            foreach (var letter in stack)
+            {
+                Console.Write(letter);
+            }
+        }
+    }
+}
