1490e509e1800c6f548b3a697637188b39167e0d
Teodor <thelad43@gmail.com>
2018-05-20 20:04:37 +0300

    Added solution for problem 03. Decimal to Binary Converter

---
+++ 01. Stacks and Queues - Lab/03. Decimal to Binary Converter/StartUp.cs
@@ -0,0 +1,52 @@
+﻿namespace _03._Decimal_to_Binary_Converter
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var number = int.Parse(Console.ReadLine());
+            var result = ConvertToBinary(number);
+            Console.WriteLine(result);
+        }
+
+        private static string ConvertToBinary(int number)
+        {
+            if (number == 0)
+            {
+                return "0";
+            }
+
+            var stack = new Stack<int>();
+
+            while (true)
+            {
+                if (number == 0)
+                {
+                    break;
+                }
+
+                var reminder = number % 2;
+                stack.Push(reminder);
+                number /= 2;
+            }
+
+            var result = new StringBuilder(stack.Count);
+
+            while (true)
+            {
+                if (stack.Count == 0)
+                {
+                    break;
+                }
+
+                result.Append(stack.Pop());
+            }
+
+            return result.ToString();
+        }
+    }
+}
