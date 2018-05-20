9f90ca6c909d32db8485edf5a8fb4969fc943230
Teodor <thelad43@gmail.com>
2018-05-20 20:17:59 +0300

    Added solution for problem 08. Recursive Fibonacci

---
+++ 02. Stacks and Queues - Exercise/08. Recursive Fibonacci/StartUp.cs
@@ -0,0 +1,33 @@
+﻿namespace _08._Recursive_Fibonacci
+{
+    using System;
+
+    public class StartUp
+    {
+        private static long[] memoization;
+
+        public static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+            memoization = new long[n + 1];
+            var result = Fibonacci(n - 1);
+            Console.WriteLine(result);
+        }
+
+        private static long Fibonacci(int n)
+        {
+            if (n <= 1)
+            {
+                return 1;
+            }
+
+            if (memoization[n] != 0)
+            {
+                return memoization[n];
+            }
+
+            memoization[n] = Fibonacci(n - 1) + Fibonacci(n - 2);
+            return memoization[n];
+        }
+    }
+}
