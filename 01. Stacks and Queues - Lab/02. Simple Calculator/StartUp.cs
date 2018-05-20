1bbbe866b2a028c86f47e31ac479a8049b6bc297
Teodor <thelad43@gmail.com>
2018-05-20 20:03:15 +0300

    Added solution for problem 02. Simple Calculator

---
+++ 01. Stacks and Queues - Lab/02. Simple Calculator/StartUp.cs
@@ -0,0 +1,35 @@
+﻿namespace _02._Simple_Calculator
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var inputLine = Console.ReadLine();
+            var stack = new Stack<string>(inputLine.Split(' ').Reverse());
+
+            while (true)
+            {
+                if (stack.Count == 1)
+                {
+                    break;
+                }
+
+                var firstNumber = int.Parse(stack.Pop());
+                var op = stack.Pop();
+                var secondNumber = int.Parse(stack.Pop());
+
+                var currentResult = 0;
+
+                currentResult = op == "+" ? firstNumber + secondNumber : firstNumber - secondNumber;
+
+                stack.Push(currentResult.ToString());
+            }
+
+            Console.WriteLine(stack.Peek());
+        }
+    }
+}
