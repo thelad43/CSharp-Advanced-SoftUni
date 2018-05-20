30daae282d72a27131c195226180d050ac92bdd7
Teodor <thelad43@gmail.com>
2018-05-20 20:07:26 +0300

    Added solution for problem 06. Traffic Jam

---
+++ 01. Stacks and Queues - Lab/06. Traffic Jam/StartUp.cs
@@ -0,0 +1,47 @@
+﻿namespace _06._Traffic_Jam
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var numberOfCarsCanPassDuringGreenLight = int.Parse(Console.ReadLine());
+            var cars = new Queue<string>();
+            var passedCars = 0;
+
+            while (true)
+            {
+                var line = Console.ReadLine();
+
+                if (line == "end")
+                {
+                    break;
+                }
+
+                if (line != "green")
+                {
+                    var car = line;
+                    cars.Enqueue(car);
+                }
+                else
+                {
+                    for (int i = 0; i < numberOfCarsCanPassDuringGreenLight; i++)
+                    {
+                        if (cars.Count == 0)
+                        {
+                            break;
+                        }
+
+                        var car = cars.Dequeue();
+                        passedCars++;
+                        Console.WriteLine($"{car} passed!");
+                    }
+                }
+            }
+
+            Console.WriteLine($"{passedCars} cars passed the crossroads.");
+        }
+    }
+}
