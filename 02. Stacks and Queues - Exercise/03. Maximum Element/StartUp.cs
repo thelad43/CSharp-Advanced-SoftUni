ac0ab74d8e06e770011d31dc9dad24c56b639ff7
Teodor <thelad43@gmail.com>
2018-05-20 20:13:11 +0300

    Added solution for problem 03. Maximum Element

---
+++ 02. Stacks and Queues - Exercise/03. Maximum Element/StartUp.cs
@@ -0,0 +1,50 @@
+﻿namespace _03._Maximum_Element
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+            var stack = new Stack<int>();
+            var maxStack = new Stack<int>();
+
+            maxStack.Push(int.MinValue);
+
+            for (int i = 0; i < n; i++)
+            {
+                var commandTokens = Console.ReadLine()
+                    .Split(' ')
+                    .Select(int.Parse)
+                    .ToArray();
+
+                if (commandTokens[0] == 1)
+                {
+                    var elementToPush = commandTokens[1];
+                    stack.Push(elementToPush);
+
+                    if (elementToPush > maxStack.Peek())
+                    {
+                        maxStack.Push(elementToPush);
+                    }
+                }
+                else if (commandTokens[0] == 2)
+                {
+                    var elementToPop = stack.Pop();
+
+                    if (maxStack.Peek() == elementToPop)
+                    {
+                        maxStack.Pop();
+                    }
+                }
+                else
+                {
+                    Console.WriteLine(maxStack.Peek());
+                }
+            }
+        }
+    }
+}
