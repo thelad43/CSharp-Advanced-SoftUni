29082fec9656b85e798a80ca5133867afa43b616
Teodor <thelad43@gmail.com>
2018-05-20 20:16:59 +0300

    Added solution for problem 07. Balanced Parenthesis

---
+++ 02. Stacks and Queues - Exercise/07. Balanced Parenthesis/StartUp.cs
@@ -0,0 +1,52 @@
+﻿namespace _07._Balanced_Parenthesis
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var input = Console.ReadLine().ToCharArray();
+
+            if (input.Length % 2 == 1)
+            {
+                Console.WriteLine("NO");
+                return;
+            }
+
+            var opening = new[] { '(', '[', '{' };
+            var closing = new[] { ')', ']', '}' };
+
+            var stack = new Stack<char>();
+
+            foreach (var element in input)
+            {
+                if (opening.Contains(element))
+                {
+                    stack.Push(element);
+                }
+                else if (closing.Contains(element))
+                {
+                    var lastElement = stack.Pop();
+
+                    if (Array.IndexOf(opening, lastElement) != Array.IndexOf(closing, element))
+                    {
+                        Console.WriteLine("NO");
+                        return;
+                    }
+                }
+            }
+
+            if (stack.Any())
+            {
+                Console.WriteLine("NO");
+            }
+            else
+            {
+                Console.WriteLine("YES");
+            }
+        }
+    }
+}
