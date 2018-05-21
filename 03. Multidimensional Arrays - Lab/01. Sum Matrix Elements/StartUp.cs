fb4220ea62a290519c0b023d495c9f1c83e50add
Teodor <thelad43@gmail.com>
2018-05-21 13:03:55 +0300

    Added solution for problem 01. Sum Matrix Elements

---
+++ 03. Multidimensional Arrays - Lab/01. Sum Matrix Elements/StartUp.cs
@@ -0,0 +1,36 @@
+﻿namespace _01._Sum_Matrix_Elements
+{
+    using System;
+    using System.Linq;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var matrixSize = Console.ReadLine()
+                .Split(new char[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            var matrix = new int[matrixSize[1]][];
+            var sum = 0;
+
+            for (int row = 0; row < matrixSize[0]; row++)
+            {
+                var numbers = Console.ReadLine()
+                    .Split(new char[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+
+                for (int col = 0; col < numbers.Length; col++)
+                {
+                    sum += numbers[col];
+                }
+            }
+
+            Console.WriteLine(matrixSize[0]);
+            Console.WriteLine(matrixSize[1]);
+            Console.WriteLine(sum);
+        }
+    }
+}
