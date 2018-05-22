e4d5bb2832ef38cae19ea79d6e54ada12ce5e657
Teodor <thelad43@gmail.com>
2018-05-22 21:06:40 +0300

    Added solution for problem 07. Lego Blocks

---
+++ 04. Multidimensional Arrays - Exercise/07. Lego Blocks/StartUp.cs
@@ -0,0 +1,63 @@
+﻿namespace _07._Lego_Blocks
+{
+    using System;
+    using System.Linq;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var rows = int.Parse(Console.ReadLine());
+            var leftBlock = new int[rows][];
+            var rightBlock = new int[rows][];
+
+            for (int row = 0; row < rows; row++)
+            {
+                leftBlock[row] = Console.ReadLine()
+                    .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+            }
+
+            for (int row = 0; row < rows; row++)
+            {
+                rightBlock[row] = Console.ReadLine()
+                    .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .Reverse()
+                    .ToArray();
+            }
+
+            var rowLenght = leftBlock[0].Length + rightBlock[0].Length;
+            var allElements = 0;
+            var isMatch = true;
+
+            for (int row = 0; row < rows; row++)
+            {
+                var currentLenght = leftBlock[row].Length + rightBlock[row].Length;
+
+                if (currentLenght != rowLenght)
+                {
+                    isMatch = false;
+                }
+
+                allElements += currentLenght;
+            }
+
+            if (isMatch)
+            {
+                var resultRow = new int[rowLenght];
+
+                for (int r = 0; r < rows; r++)
+                {
+                    resultRow = leftBlock[r].Concat(rightBlock[r]).ToArray();
+                    Console.WriteLine($"[{string.Join(", ", resultRow)}]");
+                }
+            }
+            else
+            {
+                Console.WriteLine($"The total number of cells is: {allElements}");
+            }
+        }
+    }
+}
