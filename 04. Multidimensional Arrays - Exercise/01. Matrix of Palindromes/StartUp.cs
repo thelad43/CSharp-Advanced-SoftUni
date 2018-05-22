adac9b443dd50303816b07eebcfc2c3a0699ad01
Teodor <thelad43@gmail.com>
2018-05-22 20:57:49 +0300

    Added solution for problem 01. Matrix of Palindromes

---
+++ 04. Multidimensional Arrays - Exercise/01. Matrix of Palindromes/StartUp.cs
@@ -0,0 +1,39 @@
+﻿namespace _01._Matrix_of_Palindromes
+{
+    using System;
+    using System.Linq;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var rowsAndCols = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToArray();
+
+            var rows = rowsAndCols[0];
+            var cols = rowsAndCols[1];
+            var alphabet = "abcdefghijklmnopqrstuvwxyz".ToCharArray();
+            var matrix = new string[rows, cols];
+
+            for (int row = 0; row < rows; row++)
+            {
+                for (int col = 0; col < cols; col++)
+                {
+                    matrix[row, col] = "" + (char)('a' + row) + (char)('a' + row + col) + (char)('a' + row);
+                }
+            }
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    Console.Write($"{matrix[row, col]} ");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
