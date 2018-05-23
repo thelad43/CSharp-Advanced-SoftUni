1cc65c4b5e1fe5cac57bfe74aab4df966a1a80f9
Teodor <thelad43@gmail.com>
2018-05-23 13:15:41 +0300

    Added solution for problem 02. Sum Numbers

---
+++ 05. Functional Programming - Lab/02. Sum Numbers/StartUp.cs
@@ -0,0 +1,20 @@
+﻿namespace _02._Sum_Numbers
+{
+    using System;
+    using System.Linq;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var numbers = Console.ReadLine()
+              .Split(new char[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries)
+              .Select(int.Parse)
+              .OrderBy(n => n)
+              .ToArray();
+
+            Console.WriteLine(numbers.Length);
+            Console.WriteLine(numbers.Sum());
+        }
+    }
+}
