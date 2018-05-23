26d7b5cac58c8dd33ee56ca224ef7bc67e0418cb
Teodor <thelad43@gmail.com>
2018-05-23 13:14:48 +0300

    Added solution for problem 01. Sort Even Numbers

---
+++ 05. Functional Programming - Lab/01. Sort Even Numbers/StartUp.cs
@@ -0,0 +1,20 @@
+﻿namespace _01._Sort_Even_Numbers
+{
+    using System;
+    using System.Linq;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var result = Console.ReadLine()
+                .Split(new char[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .Where(n => n % 2 == 0)
+                .OrderBy(n => n)
+                .ToArray();
+
+            Console.WriteLine(string.Join(", ", result));
+        }
+    }
+}
