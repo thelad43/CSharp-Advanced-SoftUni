495fe17a1c7a8a0e8d503a9c1e484cbd6b4dac62
Teodor <thelad43@gmail.com>
2018-05-23 13:17:01 +0300

    Added solution for problem 03. Count Uppercase Words

---
+++ 05. Functional Programming - Lab/03. Count Uppercase Words/StartUp.cs
@@ -0,0 +1,18 @@
+﻿namespace _03._Count_Uppercase_Words
+{
+    using System;
+    using System.Linq;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var result = Console.ReadLine()
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Where(w => char.IsUpper(w[0]))
+                .ToArray();
+
+            Console.WriteLine(string.Join(Environment.NewLine, result));
+        }
+    }
+}
