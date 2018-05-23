7f7c8c3c8b58ac7ec518e611c5f868c9239dd11d
Teodor <thelad43@gmail.com>
2018-05-23 13:18:03 +0300

    Added solution for problem 04. Add VAT

---
+++ 05. Functional Programming - Lab/04. Add VAT/StartUp.cs
@@ -0,0 +1,20 @@
+﻿namespace _04._Add_VAT
+{
+    using System;
+    using System.Linq;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var result = Console.ReadLine()
+                .Split(new char[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(double.Parse)
+                .Select(n => n += n * 0.2)
+                .Select(n => $"{n:F2}")
+                .ToArray();
+
+            Console.WriteLine(string.Join(Environment.NewLine, result));
+        }
+    }
+}
