27a8913847e32ed85609a3f36053e72995fb6992
Teodor <thelad43@gmail.com>
2018-05-24 20:44:17 +0300

    Added solution for problem 06. Reverse And Exclude

---
+++ 06. Functional Programming - Exercises/06. Reverse And Exclude/StartUp.cs
@@ -0,0 +1,24 @@
+﻿namespace _06._Reverse_And_Exclude
+{
+    using System;
+    using System.Linq;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var numbers = Console.ReadLine().
+                Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            var divider = int.Parse(Console.ReadLine());
+
+            numbers
+                .Where(n => n % divider != 0)
+                .Reverse()
+                .ToList()
+                .ForEach(n => Console.Write($"{n} "));
+        }
+    }
+}
