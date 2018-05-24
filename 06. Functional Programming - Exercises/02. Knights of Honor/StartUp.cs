7d7911521af84707c0f09838be4be8fabe6ef4e5
Teodor <thelad43@gmail.com>
2018-05-24 20:32:45 +0300

    Added solution for problem 02. Knights of Honor

---
+++ 06. Functional Programming - Exercises/02. Knights of Honor/StartUp.cs
@@ -0,0 +1,18 @@
+﻿namespace _02._Knights_of_Honor
+{
+    using System;
+    using System.Linq;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var names = Console.ReadLine()
+               .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+               .Select(n => $"Sir {n}")
+               .ToArray();
+
+            Console.WriteLine(string.Join(Environment.NewLine, names));
+        }
+    }
+}
