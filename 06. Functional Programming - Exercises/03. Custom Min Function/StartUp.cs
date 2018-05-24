0cfe9353968fa8886a3d48dba4b85673d8007e50
Teodor <thelad43@gmail.com>
2018-05-24 20:37:21 +0300

    Added solution for problem 03. Custom Min Function

---
+++ 06. Functional Programming - Exercises/03. Custom Min Function/StartUp.cs
@@ -0,0 +1,18 @@
+﻿namespace _03._Custom_Min_Function
+{
+    using System;
+    using System.Linq;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var smallestNumber = Console.ReadLine()
+                .Split(new char[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .Min();
+
+            Console.WriteLine(smallestNumber);
+        }
+    }
+}
