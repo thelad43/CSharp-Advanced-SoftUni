2e07fc1819060a0bd03bf387076e3d98a6931d5b
Teodor <thelad43@gmail.com>
2018-05-24 20:31:37 +0300

    Added solution for problem 01. Action Print

---
+++ 06. Functional Programming - Exercises/01. Action Print/StartUp.cs
@@ -0,0 +1,20 @@
+﻿namespace _01._Action_Print
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var names = Console.ReadLine()
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            Action<string> printNameOnNewLine = (string name) => Console.WriteLine(name);
+
+            foreach (var name in names)
+            {
+                printNameOnNewLine(name);
+            }
+        }
+    }
+}
