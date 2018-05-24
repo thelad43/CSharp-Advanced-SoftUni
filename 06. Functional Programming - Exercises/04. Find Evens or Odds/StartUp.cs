f98efbd6abc3c9c0d20efa8d8c4345dfc1582f27
Teodor <thelad43@gmail.com>
2018-05-24 20:42:28 +0300

    Added solution for problem 04. Find Evens or Odds

---
+++ 06. Functional Programming - Exercises/04. Find Evens or Odds/StartUp.cs
@@ -0,0 +1,27 @@
+﻿namespace _04._Find_Evens_or_Odds
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var range = Console.ReadLine().Split(new char[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            var evenOrOdd = Console.ReadLine();
+            var result = new List<int>();
+            Predicate<int> isEven = n => n % 2 == 0;
+
+            Enumerable.Range(range[0], range[1] - range[0] + 1)
+                .Where(n => evenOrOdd == "even" ? isEven(n) : !isEven(n))
+                .ToList()
+                .ForEach(result.Add);
+
+            Console.WriteLine(String.Join(" ", result));
+        }
+    }
+}
