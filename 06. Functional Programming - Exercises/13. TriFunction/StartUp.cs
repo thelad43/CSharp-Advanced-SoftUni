9a09a51e04f62c4278472476f4fd0bca0b0ebe21
Teodor <thelad43@gmail.com>
2018-05-24 20:56:47 +0300

    Added solution for problem 13. TriFunction

---
+++ 06. Functional Programming - Exercises/13. TriFunction/StartUp.cs
@@ -0,0 +1,34 @@
+﻿namespace _13._TriFunction
+{
+    using System;
+    using System.Linq;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var n = long.Parse(Console.ReadLine());
+
+            var names = Console.ReadLine().
+              Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            var result = names
+                .FirstOrDefault(name => CalculateSumOfCharacters(name) >= n);
+
+            Console.WriteLine(result);
+        }
+
+        private static long CalculateSumOfCharacters(string word)
+        {
+            var chars = word.ToCharArray();
+            var sum = 0L;
+
+            for (int i = 0; i < chars.Length; i++)
+            {
+                sum += chars[i];
+            }
+
+            return sum;
+        }
+    }
+}
