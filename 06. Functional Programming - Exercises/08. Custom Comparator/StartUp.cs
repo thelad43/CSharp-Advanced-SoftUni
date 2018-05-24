bbc2cb28decf899a118e7d0c16f95b1ae0af7035
Teodor <thelad43@gmail.com>
2018-05-24 20:46:17 +0300

    Added solution for problem 08. Custom Comparator

---
+++ 06. Functional Programming - Exercises/08. Custom Comparator/StartUp.cs
@@ -0,0 +1,25 @@
+﻿namespace _08._Custom_Comparator
+{
+    using System;
+    using System.Linq;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var numbers = Console.ReadLine()
+               .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+               .Select(int.Parse)
+               .ToArray();
+
+            var result = numbers
+                .Where(n => Math.Abs(n) % 2 == 0)
+                .OrderBy(n => n)
+                .Concat(numbers
+                .Where(n => Math.Abs(n) % 2 == 1)
+                .OrderBy(n => n));
+
+            Console.WriteLine(string.Join(" ", result));
+        }
+    }
+}
