2029a5e3e090f101aad4c46e4c3c0daa2426ddc4
Teodor <thelad43@gmail.com>
2018-05-24 20:47:32 +0300

    Added solution for problem 09. List Of Predicates

---
+++ 06. Functional Programming - Exercises/09. List Of Predicates/StartUp.cs
@@ -0,0 +1,39 @@
+﻿namespace _09._List_Of_Predicates
+{
+    using System;
+    using System.Linq;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var endOfRange = int.Parse(Console.ReadLine());
+
+            var numbers = Console.ReadLine()
+              .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+              .Select(int.Parse)
+              .ToArray();
+
+            var isDivisible = true;
+
+            for (int i = 1; i <= endOfRange; i++)
+            {
+                for (int j = 0; j < numbers.Length; j++)
+                {
+                    if (i % numbers[j] != 0)
+                    {
+                        isDivisible = false;
+                        break;
+                    }
+                }
+
+                if (isDivisible)
+                {
+                    Console.Write($"{i} ");
+                }
+
+                isDivisible = true;
+            }
+        }
+    }
+}
