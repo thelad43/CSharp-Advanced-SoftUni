31cabc7bf7a6f64c5d58c9c6c4f2290e97ba6dff
Teodor <thelad43@gmail.com>
2018-05-24 20:45:18 +0300

    Added solution for problem 07. Predicate For Names

---
+++ 06. Functional Programming - Exercises/07. Predicate For Names/StartUp.cs
@@ -0,0 +1,21 @@
+﻿namespace _07._Predicate_For_Names
+{
+    using System;
+    using System.Linq;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var length = int.Parse(Console.ReadLine());
+
+            var names = Console.ReadLine()
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            names
+                .Where(n => n.Length <= length)
+                .ToList()
+                .ForEach(n => Console.WriteLine(n));
+        }
+    }
+}
