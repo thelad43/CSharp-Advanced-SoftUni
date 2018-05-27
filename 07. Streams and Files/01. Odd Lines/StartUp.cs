45d2ecd63b26befc60fd903a5b1ef2d481f69f51
Teodor <thelad43@gmail.com>
2018-05-27 12:16:36 +0300

    Added solution for problem 01. Odd Lines

---
+++ 07. Streams and Files/01. Odd Lines/StartUp.cs
@@ -0,0 +1,35 @@
+﻿namespace _01._Odd_Lines
+{
+    using System;
+    using System.IO;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var path = @"..\..\..\..\Source\text.txt";
+
+            using (var reader = new StreamReader(path))
+            {
+                var index = 0;
+
+                while (true)
+                {
+                    var line = reader.ReadLine();
+
+                    if (line == null)
+                    {
+                        break;
+                    }
+
+                    if (index % 2 == 1)
+                    {
+                        Console.WriteLine(line);
+                    }
+
+                    index++;
+                }
+            }
+        }
+    }
+}
