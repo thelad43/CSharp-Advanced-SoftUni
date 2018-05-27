c634461c6eba82cf891798cd1c3c623fa0ceed1b
Teodor <thelad43@gmail.com>
2018-05-27 12:19:17 +0300

    Added solution for problem 04. Copy Binary File

---
+++ 07. Streams and Files/04. Copy Binary File/StartUp.cs
@@ -0,0 +1,33 @@
+﻿namespace _04._Copy_Binary_File
+{
+    using System.IO;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var sourcePath = @"..\..\..\..\Source\copyMe.png";
+            var destinationPath = @"..\..\..\..\Source\copiedResult.png";
+
+            using (var source = new FileStream(sourcePath, FileMode.Open))
+            {
+                using (var destination = new FileStream(destinationPath, FileMode.Create))
+                {
+                    var buffer = new byte[source.Length];
+
+                    while (true)
+                    {
+                        var bytes = source.Read(buffer, 0, buffer.Length);
+
+                        if (bytes == 0)
+                        {
+                            break;
+                        }
+
+                        destination.Write(buffer, 0, bytes);
+                    }
+                }
+            }
+        }
+    }
+}
