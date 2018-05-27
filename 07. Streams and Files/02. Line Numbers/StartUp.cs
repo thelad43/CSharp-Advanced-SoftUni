5c2333ad2a719e2fc178f4d64a45c23a850d3755
Teodor <thelad43@gmail.com>
2018-05-27 12:17:34 +0300

    Added solution for problem 02. Line Numbers

---
+++ 07. Streams and Files/02. Line Numbers/StartUp.cs
@@ -0,0 +1,38 @@
+﻿namespace _02._Line_Numbers
+{
+    using System;
+    using System.IO;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var sourcePath = @"..\..\..\..\Source\text.txt";
+            var resultPath = @"..\..\..\..\Source\result.txt";
+
+            using (var reader = new StreamReader(sourcePath))
+            {
+                using (var writer = new StreamWriter(resultPath))
+                {
+                    var index = 1;
+
+                    while (true)
+                    {
+                        var line = reader.ReadLine();
+
+                        if (line == null)
+                        {
+                            break;
+                        }
+
+                        writer.WriteLine($"Line {index}: {line}");
+                        index++;
+                    }
+                }
+            }
+
+            var result = File.ReadAllLines(resultPath);
+            Console.WriteLine(string.Join(Environment.NewLine, result));
+        }
+    }
+}
